77dcc4b5a6709e2210bce12b340d15ac4018d190
Gaston095 <56091876+Gaston095@users.noreply.github.com>
2021-05-27 17:59:58 -0300

    Add files via upload
    primeros cambios

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#menu-bar');
+let menu_tragos = document.querySelector('#menu__tragos');
+
+menu_bar.addEventListener('click',function(){
+	menu.classList.toggle('menu-toggle');
+})
+
+menu_tragos.addEventListener('click',function() {
+	menu.classList.toggle('menu-toggle');
+})
+
+
+
+
